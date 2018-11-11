55ed166161a00252ebe3e3a04f7df3f57c2d07cb
Pavel Shushvar <Pavel0556@gmail.com>
2018-11-11 22:28:11 +0300

    11.11.2018

---
+++ Lesson5-easy-1.py
@@ -0,0 +1,36 @@
+# Задача-1:
+# Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
+# из которой запущен данный скрипт.
+# И второй скрипт, удаляющий эти папки.
+
+import os
+name = ()
+number = []
+
+def new_directory (name,number):
+    name = input("Введите название директории - ")
+    number = int(input("Введите кол-во новых директорий - "))
+    try:
+        for i in range(1,number):
+            os.makedirs("{}//{}_{}".format(os.getcwd(),name,i))
+    except:
+        FileExistsError
+        print("Директория уже существует")
+
+print (new_directory(name,number))
+
+
+
+
+# Удаление созданных директорий
+def delete_directory (name):
+    try:
+        name = input("Укажие полный пусть удаляемой папки")
+        os.rmdir(name)
+        print("Папка удалена")
+    except:
+        FileNotFoundError
+        print("Такой папки нет")
+
+
+print(delete_directory(name))
---
+++ Lesson5-easy-2.py
@@ -0,0 +1,15 @@
+# Задача-2:
+# Напишите скрипт, отображающий папки текущей директории.
+import os
+main_path = ()
+
+def list_dir(main_path):
+    main_path = input("Введите путь директории: ")
+    for i in os.listdir(main_path):
+        if os.path.isfile(i) == False:
+            print(i)
+        else:
+            continue
+
+
+print(list_dir(main_path))
